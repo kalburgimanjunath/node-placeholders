121ea12119ec6249830247d34efed5bf68a7280c
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-11-05 12:12:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-uhjrlb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-uhjrlb)
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const users = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+      geo: {
+        lat: '-37.3159',
+        lng: '81.1496',
+      },
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+      name: 'Romaguera-Crona',
+      catchPhrase: 'Multi-layered client-server neural-net',
+      bs: 'harness real-time e-markets',
+    },
+  },
+];
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/api/users', (req, res) => {
+  res.send(users);
+});
+app.get('/post', (req, res) => {
+  res.send('Got post request');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
